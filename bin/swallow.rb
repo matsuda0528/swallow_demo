814f75eb3cdb41c3061e88024a342ff1b41db1b8
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2020-10-02 12:49:06 +0900

    Make swallow.rb template

--- bin/swallow.rb
+++ bin/swallow.rb
@@ -1,6 +1,17 @@
-#include cyllabus infomation
-#cyllabus_info = cyllabus parse
-#cnf_file = generate cnf file(cyllabus_info)
-#output = call sat solver(cnf_file)
-#json = decode(output)
-#print fullcalendar(json)
+$:.unshift(File.join(File.dirname(__FILE__),'..','lib'))
+
+require 'cyllabus'
+require 'encode'
+require 'solver'
+require 'decode'
+cyllabus = Cyllabus.new("./../src/cyllabus.csv") 
+#FIXME: cyllabus.csvのパス指定の方法を可用性のあるコードに変更
+#__FILE__を使えば良さそう
+
+encoder = Encode.new
+cnf_file_path = encoder.generate_cnf(cyllabus.filter_by_term 1)
+solver = Solve.new
+output_file_path = solver.call_qmaxsat(cnf_file_path)
+decoder = Decode.new
+json = decoder.decode(output_file_path)
+#NOTE: print fullcalendar(json)
