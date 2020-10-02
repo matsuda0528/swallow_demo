156503b13c25192d4af71989fb814b55413be524
matsuda0528 <p99y92og@s.okayama-u.ac.jp>
2020-10-02 16:52:27 +0900

    Add call_minisat method

--- lib/solver.rb
+++ lib/solver.rb
@@ -2,11 +2,13 @@ class Solve
   def initialize
   end
 
-  def call_qmaxsat(cnf_file)
+  def call_qmaxsat(cnf_file_path)
     #NOTE: return output file path
   end
 
-  def call_minisat(cnf_file)
-    #NOTE: return output file path
+  def call_minisat(cnf_file_path)
+    output_file_path = "output.mdl"#FIXME: tmpに出力する
+    system("minisat #{cnf_file_path} #{output_file_path}")
+    return output_file_path
   end
 end
